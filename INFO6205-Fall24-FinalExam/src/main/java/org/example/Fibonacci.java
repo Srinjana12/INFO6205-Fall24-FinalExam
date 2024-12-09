46c285085d5a8b1e9b43a6d44528d3dfb9899bf0
Srinjana Nag <156874212+Srinjana12@users.noreply.github.com>
2024-12-09 15:34:03 -0500

    code updated for final exam

---
+++ INFO6205-Fall24-FinalExam/src/main/java/org/example/Fibonacci.java
@@ -0,0 +1,49 @@
+package org.example;
+
+public class Fibonacci {
+
+    /**
+     * Return the <code>n</code>th Fibonacci number.
+     *
+     * @param n the index into the Fibonacci series. <code>n</code> is positive and
+     *          less than 93.
+     * @return if <code>n</code> < 0 or <code>n</code> > 92 then -1;
+     *         else if <code>n</code> < 2 then <code>n</code>;
+     *         else <code>fibN(n-1)</code> + <code>fibN(n-2)</code>.
+     */
+    public long fibN(int n) {
+        if (n < 0 || n > 92) {
+            return -1L;
+        }
+        if (n < 2) {
+            return n;
+        }
+        if (memo[n] != 0) {
+            return memo[n];
+        }
+        memo[n] = fibN(n - 1) + fibN(n - 2);
+        return memo[n];
+    }
+
+    /**
+     * Determine the sum of the first <code>n</code> Fibonacci numbers.
+     *
+     * @param n the index into the Fibonacci series. <code>n</code> is positive and
+     *          less than 93.
+     * @return the sum of the Fibonacci numbers 0 through n.
+     */
+    public long sumN(int n) {
+        if (n < 0 || n > 92) {
+            return -1L;
+        }
+        return fibN(n + 2) - 1;
+    }
+
+    public Fibonacci() {
+
+        memo[0] = 0;
+        memo[1] = 1;
+    }
+
+    private final long[] memo = new long[93];
+}
